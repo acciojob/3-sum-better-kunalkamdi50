175f7fd8f50faf7a9abc6345b8f2d581bcad709e
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-11 18:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function threeSum(arr, target) {
-// write your code herefunction threeSum(nums, target) {
-nums.sort((a, b) => a - b); // sort the array in ascending order
+// write your code herefunction threeSum(arr, target) {
+arr.sort((a, b) => a - b); // sort the array in ascending order
 let closestSum = Infinity; // initialize the closest sum to a large value
-for (let i = 0; i < nums.length - 2; i++) {
+for (let i = 0; i < arr.length - 2; i++) {
 let left = i + 1;
-let right = nums.length - 1;
+let right = arr.length - 1;
 while (left < right) {
-let sum = nums[i] + nums[left] + nums[right];
+let sum = arr[i] + arr[left] + arr[right];
 if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
 closestSum = sum; // update the closest sum
 }
